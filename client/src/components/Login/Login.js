a8719c08e874b878b5c40bae1c8da2df3199b9c4
Erin <erin.eliz.costello@gmail.com>
2019-11-20 16:22:18 +0100

    new login page with and new logos

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { NavLink } from "react-router-dom";
 import './Login.css';
-import wishListLogo from '../../assets/Finished-Logo.png';
+import wishListLogo from '../../assets/default.png';
 
 
 export default function Login () {
   return(
-    <div>
+    <div className="login">
       <img src={wishListLogo} alt="wishListLogo" className="wishListLogo"/>
       <NavLink to="/dashboard">
         <button className='login-btn'>Login with Google</button>
